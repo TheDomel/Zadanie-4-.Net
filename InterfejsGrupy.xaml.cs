64b9123a2b8070ce82cf21defdb43ee3aaa6eb53
Dominik <thedomelyolo@gmail.com>
2022-06-25 22:58:14 +0200

    Dodaj pliki projektów.

---
+++ InterfejsGrupy.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Zadanie_4
+{
+    /// <summary>
+    /// Logika interakcji dla klasy InterfejsGrupy.xaml
+    /// </summary>
+    public partial class InterfejsGrupy : Window
+    {
+        public InterfejsGrupy(XmlNode kategorie)
+        {
+            DataContext = kategorie;
+            InitializeComponent();
+        }
+        private void Widok(object sender, RoutedEventArgs e)
+        {
+            ListBox lista = (ListBox)FindName("Pojazdy");
+            XmlNode blok = (XmlNode)lista.SelectedItem;
+            if (blok != null)
+                new InterfejsAut(blok).Show();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Zadanie_4
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Widok(object sender, RoutedEventArgs e)
+        {
+            ListBox lista = (ListBox)FindName("kategorie");
+            XmlNode kategorie = (XmlNode)lista.SelectedItem;
+            if (kategorie != null)
+                new InterfejsGrupy(kategorie).Show();
+        }
+    }
+}
